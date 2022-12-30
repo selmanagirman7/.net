2a22a7c54df9d40350bc13b913bbc4cc17bf4ce1
selman <88801652+selmanagirman7@users.noreply.github.com>
2022-12-30 17:24:57 +0300

    Add files via upload

---
+++ Models/CategoryAdd.cs
@@ -0,0 +1,13 @@
+﻿namespace WebUI.Models
+{
+    public class CategoryAdd
+    {
+        public string? Name { get; set; }
+        public string? NameEng { get; set; }
+        public string? NameDe { get; set; }
+        public IFormFile ImagePath { get; set; }
+        public bool Statu { get; set; }
+        public DateTime CreateDate { get; set; }
+
+    }
+}
--- Models/ProductAdd.cs
+++ Models/ProductAdd.cs
@@ -1,15 +1,53 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace WebUI.Models
+﻿namespace WebUI.Models
 {
     public class ProductAdd
     {
-        public string? Stok { get; set; }
-        public string? Name { get; set; }
-        public string? Brand { get; set; }
-        public int Price { get; set; }
-        public string? Kdv { get; set; }
-        public IFormFile ImageUrl { get; set; }
 
+        public int CategoryId { get; set; }
+        public string? ProductName { get; set; }
+        public string? ProductNameEng { get; set; }
+        public string? ProductNameDe { get; set; }
+        public string? ProductDescription { get; set; }
+        public string? ProductDescriptionDe { get; set; }
+        public string? ProductDescriptionEng { get; set; }
+        public string? Ozellik1 { get; set; }
+        public string? Ozellik2 { get; set; }
+        public string? Ozellik3 { get; set; }
+        public string? Ozellik4 { get; set; }
+        public string? Ozellik5 { get; set; }
+        public string? Ozellik1Eng  { get; set; }
+        public string? Ozellik2Eng { get; set; }
+        public string? Ozellik3Eng { get; set; }
+        public string? Ozellik4Eng { get; set; }
+        public string? Ozellik5Eng { get; set; }
+        public string? Ozellik1De { get; set; }
+        public string? Ozellik2De { get; set; }
+        public string? Ozellik3De { get; set; }
+        public string? Ozellik4De { get; set; }
+        public string? Ozellik5De { get; set; }
+        public string? OzellikDeger1 { get; set; }
+        public string? OzellikDeger2 { get; set; }
+        public string? OzellikDeger3 { get; set; }
+        public string? OzellikDeger4 { get; set; }
+        public string? OzellikDeger5 { get; set; }
+        public string? OzellikDeger1Eng { get; set; }
+        public string? OzellikDeger2Eng { get; set; }
+        public string? OzellikDeger3Eng { get; set; }
+        public string? OzellikDeger4Eng { get; set; }
+        public string? OzellikDeger5Eng { get; set; }
+        public string? OzellikDeger1De { get; set; }
+        public string? OzellikDeger2De { get; set; }
+        public string? OzellikDeger3De { get; set; }
+        public string? OzellikDeger4De { get; set; }
+        public string? OzellikDeger5De { get; set; }
+        public string? Price { get; set; }
+        public string? Quantity { get; set; }
+        public string? TumbDesc { get; set; }
+        public string? TumbName { get; set; }
+        public string? TumbNameDe { get; set; }
+        public string? TumbNameEng { get; set; }
+        public IFormFile? ImagePath { get; set; }
+        public string? MonyType { get; set; }
     }
 }
+
