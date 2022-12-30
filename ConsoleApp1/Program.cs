21a3e014caa9e1ff49d3fa36c5eb9aeb62616494
selman <88801652+selmanagirman7@users.noreply.github.com>
2022-12-30 17:22:18 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "128";
+            string b = "128";
+            if (a==b)
+            {
+                Console.WriteLine("Değerler Eşit");
+            }
+            else
+            {
+                Console.WriteLine("Değerşer Eşit Değil");
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
